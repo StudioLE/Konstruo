abe744b86038618227219cd83ed4b4c87bb5d111
Laurence Elsdon <git@elsdon.me>
2024-07-25 13:00:53 +0100

    refactor: Simplify update_cursor_position with ? operator

--- beach_ui/src/tools/cursor.rs
+++ beach_ui/src/tools/cursor.rs
@@ -10,31 +10,38 @@ pub struct Cursor {
     pub delta: Vec3,
 }
 
-/// <https://bevy-cheatbook.github.io/cookbook/cursor2world.html>
 pub fn update_cursor_position(
-    mut cursor: ResMut<Cursor>,
-    q_window: Query<&Window, With<PrimaryWindow>>,
-    mut cameras: Query<(&Camera, &GlobalTransform)>,
+    cursor: ResMut<Cursor>,
+    window: Query<&Window, With<PrimaryWindow>>,
+    cameras: Query<(&Camera, &GlobalTransform)>,
 ) {
-    let Some((camera, camera_transform)) =
-        cameras.iter_mut().min_by_key(|(camera, _)| camera.order)
-    else {
-        return;
-    };
-    let window = q_window.single();
-    let Some(cursor_position) = window.cursor_position() else {
-        // if the cursor is not inside the window, we can't do anything
-        return;
-    };
+    update_cursor_position_internal(cursor, window, cameras);
+}
+
+/// <https://bevy-cheatbook.github.io/cookbook/cursor2world.html>
+pub fn update_cursor_position_internal(
+    mut cursor: ResMut<Cursor>,
+    window: Query<&Window, With<PrimaryWindow>>,
+    mut cameras: Query<(&Camera, &GlobalTransform)>,
+) -> Option<()> {
     let plane = InfinitePlane3d::new(Vec3::Y);
-    let Some(ray) = camera.viewport_to_world(camera_transform, cursor_position) else {
-        // if it was impossible to compute for whatever reason; we can't do anything
-        return;
-    };
-    let Some(distance) = ray.intersect_plane(Vec3::ZERO, plane) else {
-        return;
-    };
+    let ray = get_ray(window, &mut cameras)?;
+    let distance = ray.intersect_plane(Vec3::ZERO, plane)?;
     let position = ray.get_point(distance);
     cursor.delta = position - cursor.position;
     cursor.position = position;
+    Some(())
 }
+
+fn get_ray(
+    window: Query<&Window, With<PrimaryWindow>>,
+    cameras: &mut Query<(&Camera, &GlobalTransform)>
+) -> Option<Ray3d> {
+    let (camera, camera_transform) = cameras
+        .iter_mut()
+        .min_by_key(|(camera, _)| camera.order)?;
+    let cursor_position = window.get_single().ok()?.cursor_position()?;
+    camera.viewport_to_world(camera_transform, cursor_position)
+}
+
+
