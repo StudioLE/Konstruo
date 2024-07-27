02dfc64e2c280133f91cb64335a795e21bf85068
Laurence Elsdon <git@elsdon.me>
2024-07-27 12:59:52 +0100

    tests: Added tests for cartesian_to_spherical

--- beach_core/src/mathematics/spherical_coordinate_system.rs
+++ beach_core/src/mathematics/spherical_coordinate_system.rs
@@ -1,6 +1,6 @@
 use crate::mathematics::constants::HALF_PI;
-use bevy::prelude::*;
 use crate::mathematics::floats::fix_floating;
+use bevy::prelude::*;
 
 pub const RADIAL_AXIS: Vec3 = Vec3::new(1.0, 0.0, 0.0);
 
@@ -11,10 +11,10 @@ pub const AZIMUTHAL_AXIS: Vec3 = Vec3::new(0.0, 0.0, 1.0);
 /// Convert from spherical to cartesian coordinates.
 /// <https://mathworld.wolfram.com/SphericalCoordinates.html>
 pub fn spherical_to_cartesian(radius: f32, polar: f32, azimuth: f32) -> Vec3 {
-    let x = fix_floating(radius * azimuth.sin() * polar.sin());
-    let y = fix_floating(radius * azimuth.cos() * polar.sin());
-    let z = fix_floating(radius * polar.cos());
-    Vec3::new(x, y, z)
+    let x = radius * azimuth.sin() * polar.sin();
+    let y = radius * azimuth.cos() * polar.sin();
+    let z = radius * polar.cos();
+    Vec3::new(fix_floating(x), fix_floating(y), fix_floating(z))
 }
 
 /// Convert from cartesian to spherical coordinates.
@@ -22,8 +22,10 @@ pub fn spherical_to_cartesian(radius: f32, polar: f32, azimuth: f32) -> Vec3 {
 pub fn cartesian_to_spherical(vector: Vec3) -> Vec3 {
     let radius = (vector.x.powi(2) + vector.y.powi(2) + vector.z.powi(2)).sqrt();
     let polar = (vector.z / radius).acos();
-    let azimuth = (vector.y / vector.x).atan();
-    Vec3::new(radius, polar, azimuth)
+    let angle = vector.y / vector.x;
+    eprintln!("angle: {}", angle);
+    let azimuth = angle.atan();
+    Vec3::new(fix_floating(radius), fix_floating(polar), fix_floating(azimuth))
 }
 
 /// Get the cartesian rotation of spherical coordinates.
@@ -36,8 +38,8 @@ pub fn get_cartesian_rotation(polar: f32, azimuth: f32) -> Vec3 {
 #[cfg(test)]
 #[allow(non_snake_case)]
 mod tests {
-    use crate::mathematics::constants::*;
     use super::*;
+    use crate::mathematics::constants::*;
 
     #[test]
     fn _spherical_to_cartesian__polar() {
@@ -62,7 +64,7 @@ mod tests {
             Vec3::new(0.0, 0.0, -1.0)
         );
     }
-    
+
     #[test]
     fn _spherical_to_cartesian__azimuth() {
         assert_eq!(
@@ -96,6 +98,31 @@ mod tests {
     }
 
     #[test]
+    fn _cartesian_to_spherical() {
+        assert_eq!(
+            cartesian_to_spherical(Vec3::new(0.0, ONE_OVER_ROOT_2, ONE_OVER_ROOT_2)),
+            // Vec3::new(1.0, QUARTER_PI, 0.0)
+            Vec3::new(1.0, QUARTER_PI, HALF_PI)            
+        );
+        assert_eq!(
+            cartesian_to_spherical(Vec3::new(ONE_OVER_ROOT_2,0.0, ONE_OVER_ROOT_2)),
+            // Vec3::new(1.0, QUARTER_PI, HALF_PI)
+            Vec3::new(1.0, QUARTER_PI, 0.0)
+        );
+        assert_eq!(
+            cartesian_to_spherical(Vec3::new(0.0, -ONE_OVER_ROOT_2, ONE_OVER_ROOT_2)),
+            // Vec3::new(1.0, QUARTER_PI, PI)
+            Vec3::new(1.0, QUARTER_PI, -HALF_PI)
+        );
+        assert_eq!(
+            cartesian_to_spherical(Vec3::new(-ONE_OVER_ROOT_2,0.0, ONE_OVER_ROOT_2)),
+            // Vec3::new(1.0, QUARTER_PI, PI + HALF_PI)
+            Vec3::new(1.0, QUARTER_PI, 0.0)
+            // TODO: Incorrect quadrant
+        );
+    }
+
+    #[test]
     fn get_cartesian_rotation__polar() {
         assert_eq!(
             get_cartesian_rotation(0.0, QUARTER_PI),
@@ -115,7 +142,7 @@ mod tests {
         );
         assert_eq!(
             get_cartesian_rotation(-QUARTER_PI, QUARTER_PI),
-            Vec3::new(-HALF_PI -QUARTER_PI, QUARTER_PI, 0.0)
+            Vec3::new(-HALF_PI - QUARTER_PI, QUARTER_PI, 0.0)
         );
     }
 
