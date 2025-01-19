1a40d047f28a1a06173bd2dcdf0c71cdb77f14a6
Laurence Elsdon <git@elsdon.me>
2025-01-19 08:51:50 +0000

    refactor: Use required component constructors for Sun

--- beach_geography/src/sun.rs
+++ beach_geography/src/sun.rs
@@ -2,23 +2,26 @@ use bevy::prelude::*;
 use std::f32::consts::TAU;
 
 #[derive(Component)]
-#[require(DirectionalLight, Transform)]
+#[require(DirectionalLight(create_light), Transform(create_transform))]
 pub struct Sun;
 
+fn create_light() -> DirectionalLight {
+    DirectionalLight {
+        // illuminance: light_consts::lux::OVERCAST_DAY,
+        illuminance: 5000.0,
+        shadows_enabled: true,
+        ..default()
+    }
+}
+
+fn create_transform() -> Transform {
+    Transform {
+        translation: Vec3::new(0.0, 1000.0, 0.0),
+        rotation: Quat::from_rotation_x(TAU / 8.0 * -1.0),
+        ..default()
+    }
+}
+
 pub fn spawn_sun(mut commands: Commands) {
-    let bundle = (
-        Sun,
-        DirectionalLight {
-            // illuminance: light_consts::lux::OVERCAST_DAY,
-            illuminance: 5000.0,
-            shadows_enabled: true,
-            ..default()
-        },
-        Transform {
-            translation: Vec3::new(0.0, 1000.0, 0.0),
-            rotation: Quat::from_rotation_x(TAU / 8.0 * -1.0),
-            ..default()
-        }
-    );
-    commands.spawn(bundle);
+    commands.spawn(Sun);
 }
