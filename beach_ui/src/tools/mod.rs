7e7c058222c4f57359bee6294bf40ea56e4b7a50
Laurence Elsdon <git@elsdon.me>
2025-01-20 11:21:16 +0000

    feat: Disable cursor gizmo

--- beach_ui/src/tools/mod.rs
+++ beach_ui/src/tools/mod.rs
@@ -1,7 +1,6 @@
 use crate::tools::cursor::{update_cursor_position, Cursor};
 use crate::tools::gizmos::{
-    configure_gizmos, draw_cursor_gizmo, draw_origin_gizmo, Gizmos100, Gizmos300, Gizmos500,
-    Gizmos700, Gizmos900,
+    configure_gizmos, draw_origin_gizmo, Gizmos100, Gizmos300, Gizmos500, Gizmos700, Gizmos900,
 };
 use crate::tools::grid::spawn_grid;
 use bevy::app::{App, Startup, Update};
@@ -25,6 +24,6 @@ pub fn tools_plugin(app: &mut App) {
         .add_systems(Update, draw_origin_gizmo)
         // .add_systems(Update, draw_grid)
         // .add_systems(Update, draw_curve)
-        .add_systems(Update, draw_cursor_gizmo)
+        // .add_systems(Update, draw_cursor_gizmo)
         .add_systems(Update, update_cursor_position);
 }
