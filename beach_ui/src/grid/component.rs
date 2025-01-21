552a9258b76eaa2163e97341c4c7cd107e0da026
Laurence Elsdon <git@elsdon.me>
2025-01-21 19:37:51 +0000

    feat(Grid): Revise grid to have circular bounds

--- beach_ui/src/grid/component.rs
+++ beach_ui/src/grid/component.rs
@@ -4,11 +4,11 @@ use bevy::prelude::*;
 use bevy::render::mesh::PrimitiveTopology;
 use bevy::render::render_asset::RenderAssetUsages;
 
-const LENGTH: i32 = 1000;
-const SPACING: [i32; 3] = [1, 10, 100];
+const RADIUS: u32 = 2_500;
+const SPACING: [u32; 3] = [1, 10, 100];
 
 #[allow(clippy::integer_division)]
-const COUNT: i32 = LENGTH / SPACING[0] + 1;
+const COUNT: u32 = (RADIUS * 2) / SPACING[0] + 1;
 
 #[derive(Component)]
 pub(super) enum Grid {
@@ -49,32 +49,32 @@ impl Grid {
 
 fn create_lines() -> (Vec<Vec3>, Vec<Vec3>, Vec<Vec3>) {
     let range = 0..COUNT;
-    let max = LENGTH as f32 / 2.0;
-    let min = max * -1.0;
+    let radius = RADIUS as f32;
     let mut minor = Vec::new();
     let mut medium = Vec::new();
     let mut major = Vec::new();
     for i in range {
-        let x = i as f32;
-        let start_x = vec3(min + x, min, 0.0);
-        let end_x = vec3(min + x, max, 0.0);
-        let start_z = vec3(min, min + x, 0.0);
-        let end_z = vec3(max, min + x, 0.0);
+        let a = i as f32 - radius;
+        let b = (radius.powi(2) - a.powi(2)).sqrt();
+        let start_x = vec3(a, b, 0.0);
+        let end_x = vec3(a, -b, 0.0);
+        let start_y = vec3(-b, a, 0.0);
+        let end_y = vec3(b, a, 0.0);
         if i % SPACING[2] == 0 {
             major.push(start_x);
             major.push(end_x);
-            major.push(start_z);
-            major.push(end_z);
+            major.push(start_y);
+            major.push(end_y);
         } else if i % SPACING[1] == 0 {
             medium.push(start_x);
             medium.push(end_x);
-            medium.push(start_z);
-            medium.push(end_z);
+            medium.push(start_y);
+            medium.push(end_y);
         } else {
             minor.push(start_x);
             minor.push(end_x);
-            minor.push(start_z);
-            minor.push(end_z);
+            minor.push(start_y);
+            minor.push(end_y);
         }
     }
     (minor, medium, major)
