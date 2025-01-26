2e8c50605642785562aa31f848c8a4d55dcf6db5
Laurence Elsdon <git@elsdon.me>
2025-01-26 18:34:19 +0000

    refactor(TriangleStrip): Move add_vertices_by_splitting_longest_edge logic into TriangleStrip

--- src/core/geometry/triangle_strip.rs
+++ src/core/geometry/triangle_strip.rs
@@ -2,37 +2,43 @@ use bevy::asset::RenderAssetUsages;
 use bevy::math::Vec3;
 use bevy::prelude::Mesh;
 use bevy::render::mesh::PrimitiveTopology;
-use std::fmt::{Display, Formatter};
-use TriangleStripError::*;
+use std::cmp::Ordering;
 
 /// A [`PrimitiveTopology::TriangleStrip`]
 #[derive(Clone, Debug, PartialEq)]
-pub struct TriangleStip {
+pub struct TriangleStrip {
     /// Pairs of vertices
     pairs: Vec<[Vec3; 2]>,
 }
 
-#[derive(Debug, PartialEq)]
-pub enum TriangleStripError {
-    InEqual { left: usize, right: usize },
-}
-
-impl TriangleStip {
+impl TriangleStrip {
     /// Create a [`TriangleStrip`] between two parallel polylines.
     ///
-    /// The polylines must have equal number of vertices.
-    pub fn between_polylines(polylines: &[Vec<Vec3>; 2]) -> Result<Self, TriangleStripError> {
-        let left = polylines[0].len();
-        let right = polylines[1].len();
-        if left != right {
-            return Err(InEqual { left, right });
+    /// If the polylines do not have an equal vertices count then the longest edge will be split.
+    #[must_use]
+    #[allow(
+        clippy::as_conversions,
+        clippy::cast_possible_wrap,
+        clippy::cast_sign_loss
+    )]
+    pub fn between_polylines(polylines: &[Vec<Vec3>; 2]) -> Self {
+        let mut polylines = [polylines[0].clone(), polylines[1].clone()];
+        let difference = polylines[0].len() as isize - polylines[1].len() as isize;
+        match difference.cmp(&0) {
+            Ordering::Less => {
+                add_vertices_by_splitting_longest_edge(&mut polylines[0], difference.unsigned_abs());
+            }
+            Ordering::Greater => {
+                add_vertices_by_splitting_longest_edge(&mut polylines[1], difference as usize);
+            }
+            Ordering::Equal => {}
         }
         let pairs = polylines[0]
             .iter()
             .zip(polylines[1].iter())
             .map(|(&a, &b)| [a, b])
             .collect();
-        Ok(Self { pairs })
+        Self { pairs }
     }
 
     /// Create a [`PrimitiveTopology::TriangleStrip`] mesh.
@@ -47,14 +53,26 @@ impl TriangleStip {
     }
 }
 
-impl Display for TriangleStripError {
-    #[allow(clippy::absolute_paths)]
-    fn fmt(&self, formatter: &mut Formatter<'_>) -> std::fmt::Result {
-        let output = match self {
-            InEqual { left, right } => {
-                format!("Polyline lengths were not equal: {left} != {right}")
-            }
-        };
-        output.fmt(formatter)
+#[allow(clippy::indexing_slicing)]
+/// Add vertices to a polyline by splitting the longest edge.
+fn add_vertices_by_splitting_longest_edge(polyline: &mut Vec<Vec3>, count: usize) {
+    for _ in 0..count {
+        let starts = polyline.iter().take(polyline.len() - 1);
+        let ends = polyline.iter().skip(1);
+        let distances = starts
+            .zip(ends)
+            .map(|(start, end)| start.distance(*end))
+            .collect::<Vec<f32>>();
+        let max_distance = distances
+            .iter()
+            .max_by(|a, b| a.partial_cmp(b).expect("Floats are comparable"))
+            .expect("Should be at least one distance");
+        #[allow(clippy::float_cmp)]
+        let max_index = distances
+            .iter()
+            .position(|&x| x == *max_distance)
+            .expect("Item should exist");
+        let mid_point = (polyline[max_index] + polyline[max_index + 1]) / 2.0;
+        polyline.insert(max_index + 1, mid_point);
     }
 }
--- src/infrastructure/ways/surface.rs
+++ src/infrastructure/ways/surface.rs
@@ -1,6 +1,5 @@
 use crate::beziers::CubicBezierSpline;
-use crate::geometry::triangles::add_vertices_by_spliting_longest_edge;
-use crate::geometry::TriangleStip;
+use crate::geometry::TriangleStrip;
 use crate::infrastructure::ways::way::Way;
 use crate::infrastructure::ways::{WayMaterials, FLATTEN_TOLERANCE, OFFSET_ACCURACY};
 use bevy::prelude::*;
@@ -75,35 +74,17 @@ impl WaySurface {
     /// Get the polylines of each edge.
     ///
     /// The polylines will have the same number of vertices.
-    #[allow(
-        clippy::as_conversions,
-        clippy::cast_possible_wrap,
-        clippy::cast_sign_loss
-    )]
     fn get_polylines(&self, way: &Way) -> [Vec<Vec3>; 2] {
         let splines = self.get_splines(way);
-        let mut polylines = [
+        [
             splines[0].flatten(FLATTEN_TOLERANCE),
             splines[1].flatten(FLATTEN_TOLERANCE),
-        ];
-        let difference = polylines[0].len() as isize - polylines[1].len() as isize;
-        match difference.cmp(&0) {
-            Ordering::Less => {
-                add_vertices_by_spliting_longest_edge(&mut polylines[0], difference.unsigned_abs());
-            }
-            Ordering::Greater => {
-                add_vertices_by_spliting_longest_edge(&mut polylines[1], difference as usize);
-            }
-            Ordering::Equal => {}
-        }
-        polylines
+        ]
     }
 
     /// Get the [`Mesh`].
     fn get_mesh(&self, way: &Way) -> Mesh {
         let polylines = self.get_polylines(way);
-        TriangleStip::between_polylines(&polylines)
-            .expect("polylines should have equal length")
-            .to_mesh()
+        TriangleStrip::between_polylines(&polylines).to_mesh()
     }
 }
