5f70c84d23ed34a89ca2932ba85d2c07487adeb6
Laurence Elsdon <git@elsdon.me>
2025-02-05 07:33:47 +0000

    feat(distribution): Add Distribution::regenerate method

--- src/core/distribution/distribution.rs
+++ src/core/distribution/distribution.rs
@@ -15,6 +15,35 @@ pub struct Distribution {
 }
 
 impl Distribution {
+    /// Distribute the [`Distributable`] children.
+    pub fn regenerate(
+        meshes: &mut ResMut<Assets<Mesh>>,
+        distribution: &Distribution,
+        transform: &mut Transform,
+        mesh: &mut Mesh3d,
+        children: &Children,
+        distributables: &mut Query<(Entity, &Distributable, &mut Transform), Without<Distribution>>,
+    ) {
+        let (entities, items) = get_sorted_children(children, distributables);
+        let container = distribution.flex.execute(items);
+        if distribution.translate_to_ground {
+            let translation = transform.translation.with_z(container.size.z * 0.5);
+            *transform = Transform::from_translation(translation)
+                .with_rotation(transform.rotation)
+                .with_scale(transform.scale);
+        }
+        if distribution.generate_container_mesh {
+            *mesh = Mesh3d(meshes.add(Cuboid::from_size(container.size)));
+        }
+        for (entity, distributed) in entities.iter().zip(container.items) {
+            let (_, _, mut transform) = distributables.get_mut(*entity).expect("entity exists");
+            // TODO: If size is different we may need to set scale
+            *transform = Transform::from_translation(distributed.translation)
+                .with_rotation(transform.rotation)
+                .with_scale(transform.scale);
+        }
+    }
+
     /// System to create [`Mesh3d`], [`WaySurface`], and [`WayControl`] when a [`Way`] is added.
     pub fn added_system(
         mut meshes: ResMut<Assets<Mesh>>,
@@ -25,24 +54,14 @@ impl Distribution {
         mut distributables: Query<(Entity, &Distributable, &mut Transform), Without<Distribution>>,
     ) {
         for (distribution, mut transform, mut mesh, children) in distributions.iter_mut() {
-            let (entities, items) = get_sorted_children(children, &distributables);
-            let container = distribution.flex.execute(items);
-            if distribution.translate_to_ground {
-                let translation = transform.translation.with_z(container.size.z * 0.5);
-                *transform = Transform::from_translation(translation)
-                    .with_rotation(transform.rotation)
-                    .with_scale(transform.scale);
-            }
-            if distribution.generate_container_mesh {
-                *mesh = Mesh3d(meshes.add(Cuboid::from_size(container.size)));
-            }
-            for (entity, distributed) in entities.iter().zip(container.items) {
-                let (_, _, mut transform) = distributables.get_mut(*entity).expect("entity exists");
-                // TODO: If size is different we may need to set scale
-                *transform = Transform::from_translation(distributed.translation)
-                    .with_rotation(transform.rotation)
-                    .with_scale(transform.scale);
-            }
+            Self::regenerate(
+                &mut meshes,
+                distribution,
+                &mut transform,
+                &mut mesh,
+                children,
+                &mut distributables,
+            );
         }
     }
 }
