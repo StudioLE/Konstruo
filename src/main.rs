54b49dff6c08124a1f0b212a08dd5dee3d98fb25
Laurence Elsdon <git@elsdon.me>
2025-04-01 12:25:39 +0100

    build: Configure LogPlugin to show Trace logs for all except the noisiest crates

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
 use bevy::app::PluginGroupBuilder;
 use bevy::asset::AssetMetaCheck;
+use bevy::log::Level;
+use bevy::log::LogPlugin;
 use bevy::prelude::*;
 use konstruo::architecture::*;
 use konstruo::distribution::DistributionPlugin;
@@ -8,10 +10,11 @@ use konstruo::examples::*;
 use konstruo::infrastructure::*;
 use konstruo::ui::InteractionPlugin;
 use konstruo::ui::*;
+use std::collections::HashMap;
 
 fn main() {
     App::new()
-        .add_plugins(default_plugins())
+        .add_plugins(configure_default_plugins())
         .add_plugins(MeshPickingPlugin)
         .add_plugins(AxisMarkerPlugin)
         .add_plugins(ModularBuildingsPlugin)
@@ -42,23 +45,68 @@ fn main() {
         .run();
 }
 
-fn default_plugins() -> PluginGroupBuilder {
+fn configure_default_plugins() -> PluginGroupBuilder {
+    let plugins = DefaultPlugins.set(configure_log_plugin());
     if cfg!(target_arch = "wasm32") {
-        trace!("Configuring DefaultPlugins for WebAssembly");
-        DefaultPlugins
-            .set(AssetPlugin {
-                file_path: "/assets".to_owned(),
-                meta_check: AssetMetaCheck::Never,
-                ..default()
-            })
-            .set(WindowPlugin {
-                primary_window: Some(Window {
-                    canvas: Some("#primary-window".into()),
-                    ..default()
-                }),
-                ..default()
-            })
+        configure_webassembly(plugins)
     } else {
-        DefaultPlugins.build()
+        plugins
     }
+}
+
+#[allow(suspicious_double_ref_op)]
+fn configure_log_plugin() -> LogPlugin {
+    let mut filter: HashMap<&str, Vec<&str>> = HashMap::new();
+    filter.insert(
+        "info",
+        vec![
+            "bevy_render",
+            "calloop",
+            "cosmic_text",
+            "naga",
+            "offset_allocator",
+            "polling",
+            "wgpu_core",
+            "wgpu_hal",
+            "winit",
+        ],
+    );
+    filter.insert("warn", vec!["wgpu_hal"]);
+    LogPlugin {
+        level: Level::TRACE,
+        filter: create_log_filter(filter),
+        ..default()
+    }
+}
+
+#[allow(suspicious_double_ref_op)]
+fn create_log_filter(filter: HashMap<&str, Vec<&str>>) -> String {
+    filter
+        .iter()
+        .flat_map(|(level, names)| names.iter().map(|name| (level.clone(), name)))
+        .fold(String::new(), |mut acc, (level, name)| {
+            acc.push_str(name);
+            acc.push('=');
+            acc.push_str(level);
+            acc.push(',');
+            acc
+        })
+}
+
+#[allow(suspicious_double_ref_op)]
+fn configure_webassembly(plugins: PluginGroupBuilder) -> PluginGroupBuilder {
+    trace!("Configuring DefaultPlugins for WebAssembly");
+    plugins
+        .set(AssetPlugin {
+            file_path: "/assets".to_owned(),
+            meta_check: AssetMetaCheck::Never,
+            ..default()
+        })
+        .set(WindowPlugin {
+            primary_window: Some(Window {
+                canvas: Some("#primary-window".into()),
+                ..default()
+            }),
+            ..default()
+        })
 }
